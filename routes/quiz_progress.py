91e5a405392c730126e33c158bb888e61eaf1300
Aryan Ramesh Jain <138214350+jainaryan04@users.noreply.github.com>
2025-01-24 17:42:51 +0530

    username and domain submit fixes (#17)

--- routes/quiz_progress.py
+++ routes/quiz_progress.py
@@ -2,7 +2,7 @@ from fastapi import FastAPI, HTTPException, Depends
 from typing import Dict, List
 from pydantic import BaseModel
 from middleware.verifyToken import get_access_token
-from config import get_resources
+from config import user_table
 from firebase_admin import auth
 
 quiz_app=FastAPI()
@@ -15,12 +15,10 @@ class QuizProgress(BaseModel):
 
 #Route for getting quiz progress
 @quiz_app.get("/get-progress")
-async def get_progress(idToken:str=Depends(get_access_token), resources: dict = Depends(get_resources)):
+async def get_progress(idToken:str=Depends(get_access_token)):
     try:
         #Validating token and fetching user
-        user_table = resources["user_table"]
-        app = resources["firebase_app"]
-        decoded_token=auth.verify_id_token(idToken, app)
+        decoded_token=auth.verify_id_token(idToken, app=get_firebase_app())
         email=decoded_token.get('email')
         response = user_table.get_item(Key={'uid': email})
         user = response.get('Item')
